2c4550cb5a87792b94019321872f01a24cd43b75
llamato <tguessbacher@gmx.de>
2026-06-27 20:20:24 +0200

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Figherfighter c64 llvm-mos development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    llamato-dotfiles.url = "github:llamato/dotfiles";
+  };
+
+  outputs = { self, nixpkgs, llamato-dotfiles, ... } @ inputs: let
+    systems = [ "x86_64-linux" ];
+  in {
+    devShells = nixpkgs.lib.genAttrs systems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        llvm-mos = pkgs.callPackage (inputs.llamato-dotfiles + "/nixos/packages/llvm-mos/package.nix") { };
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            acme
+            llvm-mos
+            vice
+          ];
+        };
+      }
+    );
+  };
+}
